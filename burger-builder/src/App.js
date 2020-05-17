a1872485a721fe56095e9978cdca8386ca6a6a31
Mikhailo <mikhailotorsky@gmail.com>
2020-05-17 18:03:54 +0300

    File rename

--- burger-builder/src/App.js
+++ burger-builder/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Layout from './components/Layout/Layout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+      <Layout>
+        <p>Test</p>
+      </Layout>
     </div>
   );
 }
