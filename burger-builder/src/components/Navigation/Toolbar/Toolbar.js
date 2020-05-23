adc121a5ad422a90862e869b0428215904de9b42
Mikhailo <mikhailotorsky@gmail.com>
2020-05-23 17:03:19 +0300

    SideDrawer done

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -3,10 +3,11 @@ import React from 'react';
 import classes from './Toolbar.module.scss';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import SideDrawerToggle from '../SideDrawer/SideDrawerToggle/SideDrawerToggle';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>Menu</div>
+    <SideDrawerToggle open={props.toggleSideDrawer}/>
     <div className={classes.Logo}>
       <Logo />
     </div>
