a3983a542021fac1d5027566022ae60c274f6f19
Mikhailo <mikhailotorsky@gmail.com>
2020-05-24 00:25:31 +0300

    Folder structure fix

--- burger-builder/src/components/UI/Modal/Modal.js
+++ burger-builder/src/components/UI/Modal/Modal.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 import classes from './Modal.module.scss';
-import Auxiliary from '../../../hoc/Auxiliary';
+import Auxiliary from '../../../hoc/Auxiliary/Auxiliary';
 import Backdrop from '../Backdrop/Backdrop';
 
 class Modal extends Component {
   shouldComponentUpdate(nextProps, nextState ) {
     return nextProps.showModal !== this.props.showModal
   }
-  
+
   render() {
     return (
       <Auxiliary>
